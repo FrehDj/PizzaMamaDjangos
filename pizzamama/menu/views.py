01902b08d350f844290e2854210f2d6584070345
FrehDj <freh.djillali@gmail.com>
2024-04-14 15:58:51 +0100

    Menu pizza a 90%

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -7,4 +7,5 @@ def index(request):
     pizzas = Pizza.objects.all()
     pizzas_names_prices = [pizza.nom +": "+str(pizza.prix) for pizza in pizzas]
     pizzas_names_str = " DZ, ".join(pizzas_names_prices)
-    return HttpResponse("Les Pizzas : " + pizzas_names_str +" DZ")
+    #return HttpResponse("Les Pizzas : " + pizzas_names_str +" DZ")
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
